78e79b0fdafcd8083121d0afaccdedc95ced2e76
Michael Duncan <mwduncan2018@gmail.com>
2021-11-09 15:30:46 -0500

    Update

---
+++ pluralsight-1/main.tf
@@ -0,0 +1,86 @@
+# Provider
+provider "aws" {
+    access_key = "AKIAWCEO7EA62C7ONKHH"
+    secret_key = "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"
+    region = "us-east-1"
+}
+
+# Data
+data "aws_ssm_parameter" "ami" {
+    name = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
+}
+
+# Networking
+resource "aws_vpc" "vpc" {
+    cidr_block = "10.0.0.0/16"
+    enable_dns_hostnames = true
+}
+
+resource "aws_internet_gateway" "igw" {
+    vpc_id = aws_vpc.vpc.id
+}
+
+resource "aws_subnet" "subnet1" {
+    cidr_block = "10.0.0.0/24"
+    vpc_id = aws_vpc.vpc.id
+    map_public_ip_on_launch = "true"
+}
+
+# Routing
+resource "aws_route_table" "trb" {
+    vpc_id = aws_vpc.vpc.id
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.igw.id
+    }
+}
+
+resource "aws_route_table_association" "rta-subnet1" {
+    subnet_id = aws_subnet.subnet1.id
+    route_table_id = aws_route_table.trb.id  
+}
+
+# Security Groups
+resource "aws_security_group" "nginx-sg" {
+    name = "nginx_sg"
+    vpc_id = aws_vpc.vpc.id
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
+# Instances
+resource "aws_instance" "nginx1" {
+    ami = nonsensitive(data.aws_ssm_parameter.ami.value)
+    instance_type = "t2.micro"
+    subnet_id = aws_subnet.subnet1.id
+    vpc_security_group_ids = [aws_security_group.nginx_sg.id]
+
+    user_data = <<EOF
+#! /bin/bash
+sudo amazon-linux-extras install -y nginx1
+sudo service nginx start
+sudo rm /usr/share/nginx/html/index.html
+echo '<html><head><title>Duncan Terraform Demo</title></head><body><h1>Duncan Terraform Demo</h1></body></html>'
+EOF
+
+}
+
+
+
+
+
+
+
+
+
+
